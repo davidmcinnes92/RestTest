ecb38c980fd14cf5bc3f456ae88e65d635d3ac87
David McInnes <dMcInnes@fes-group.co.uk>
2021-10-01 15:27:20 +0100

    Added metadata

---
+++ src/main/java/com/example/RestTest/model/Metadata.java
@@ -0,0 +1,20 @@
+package com.example.RestTest.model;
+
+public class Metadata {
+    
+    public long id;
+    public String Description;
+
+    // Empty constructor
+    public Metadata() {
+
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
